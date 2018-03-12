b82b17425594be38bb65f6719ebbf0093964cbf6
matthew_towles <matthew.d.towles.ctr@mail.mil>
2018-03-12 12:14:28 -0400

    Three ways to choose pivot for partitioning

--- program3/QuickSort.py
+++ program3/QuickSort.py
@@ -5,18 +5,16 @@ inFile = open(INPUT_FILE, 'r')
 with inFile as f:
     inputNums = [int(integers.strip()) for integers in f.readlines()]
 
-comparisons = 0
 
 def quicksort(arr, lo, hi):
     if hi > lo:
-        global comparisons
-        pivotPosition = partition(arr, lo, hi)
-        comparisons += (hi - lo)
+        # change which parition below
+        pivotPosition = partition(arr, lo, hi, partitionmid)
         quicksort(arr, lo, pivotPosition - 1)
         quicksort(arr, pivotPosition + 1, hi)
 
 
-def partition(arr, lo, hi):
+def partitionlo(arr, lo, hi):
     pivotVal = arr[lo]
     # i is position 1 to the right of pivot
     # pivot will be moved each partition to i-1
@@ -27,7 +25,6 @@ def partition(arr, lo, hi):
             arr[j], arr[i] = arr[i], arr[j]
             i += 1
         j += 1
-
     # swap last item on left half with pivot
     # do this because pivot must be separator for left and right
     arr[lo], arr[i - 1] = arr[i - 1], arr[lo]
@@ -35,9 +32,34 @@ def partition(arr, lo, hi):
     return pivotPosition
 
 
+def partitionhi(arr, lo, hi):
+    pivotVal = arr[hi]
+    i = j = lo
+    while j < hi:
+        if arr[j] < pivotVal:
+            arr[j], arr[i] = arr[i], arr[j]
+            i += 1
+        j += 1
+    # swap first item on right half with pivot
+    arr[hi], arr[i] = arr[i], arr[hi]
+    pivotPosition = i
+    return pivotPosition
+
+
+def partitionmid(arr, lo, hi):
+    mid = int((hi - lo)//2)
+    pivotVal = sorted((arr[lo], arr[mid], arr[hi]))[1]
+    if pivotVal == arr[lo]:
+        return partitionlo(arr, lo, hi)
+    elif pivotVal == arr[hi]:
+        return partitionhi(arr, lo, hi)
+    arr[lo], arr[mid] = arr[mid], arr[lo]
+    return partitionlo(arr, lo, hi)
+
+
+def partition(arr, lo, hi, option):
+    return option(arr, lo, hi)
+
 
 quicksort(inputNums, 0, len(inputNums) - 1)
-print('Number of comparisons made: ' + str(comparisons))
-# 1. 162085
-# 2. (164123)
-# 3. (138382)
+# print(inputNums)
